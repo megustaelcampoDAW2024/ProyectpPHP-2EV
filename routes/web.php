77137bacd3f75d42df5e38a689d13695d1062503
megustaelcampo <pablobejarano2005@gmail.com>
2025-01-31 10:14:35 +0100

    Add initial setup for authentication and dashboard views - BOOTSTRAP

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use App\Http\Controllers\Ejercicios;
 use App\Http\Controllers\TareaController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/', [TareaController::class, 'logInCreate'])->name('logIn.create');
-Route::any('/logIn', [TareaController::class, 'logInStore'])->name('logIn.store');
-Route::any('/inicio', [TareaController::class, 'inicio'])->name('inicio');
+// Route::any('/', [TareaController::class, 'logInCreate'])->name('logIn.create');
+// Route::any('/logIn', [TareaController::class, 'logInStore'])->name('logIn.store');
+// Route::any('/inicio', [TareaController::class, 'inicio'])->name('inicio');
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
